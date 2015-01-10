9aa5e9ef9603b118ba58c0269279bf869398f60f
Praveen Kumar <webtunings@gmail.com>
2015-01-10 17:12:40 +0530

    string map - find and replace

---
+++ 20_string_map.tcl
@@ -0,0 +1,10 @@
+string map {p q} "praveen kumar"
+qraveen kumar
+string map {r a} "praveen kumar"
+paaveen kumaa
+string map {r a e d} "praveen kumar"
+paavddn kumaa
+string map {r a a b} "praveen kumar"
+pabveen kumba
+string map {ra ab} "praveen kumar"
+pabveen kumar
