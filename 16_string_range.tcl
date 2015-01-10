421aa663b90d36d277f0bd87c1dbf481ce31a997
Praveen Kumar <webtunings@gmail.com>
2015-01-10 15:58:15 +0530

    string range

---
+++ 16_string_range.tcl
@@ -0,0 +1,18 @@
+set name "praveen kumar"
+praveen kumar
+string range name 2 end
+me
+string range $name 2 end
+aveen kumar
+string range $name 2 3
+av
+string range $name 1 end
+raveen kumar
+string range $name 1 end-1
+raveen kuma
+string range $name 1 end-2
+raveen kum
+string range $name 1 end-3
+raveen ku
+string range $name 1 end-4
+raveen k
