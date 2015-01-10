7c5d3a4b606edf3bb7e9983ec14d7bf3f5c104e2
Praveen Kumar <webtunings@gmail.com>
2015-01-10 17:19:46 +0530

    append for fast string concatenation

---
+++ 21_append_string.tcl
@@ -0,0 +1,14 @@
+set str "praveen"
+praveen
+append $str " kumar"
+ kumar
+puts $str
+praveen
+append str " kumar"
+praveen kumar
+puts $str
+praveen kumar
+
+#same as append but append is fast
+set str "$str lastKumar"
+praveen kumar lastKumar
